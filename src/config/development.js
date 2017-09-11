7dc4a719c3c0452963ed3daebbc271ad046730c0
Melekesov Aleksey <i198622@gmail.com>
2017-09-11 21:20:13 +0400

    Dependency were updated.

--- src/config/development.js
+++ src/config/development.js
@@ -5,9 +5,9 @@ export default {
   // ACCOUNT E-MAIL
   username: 'admin@example.com', // admin@example.com
   // API KEY
-  apiKey: '9257E7A0PS02MN1i02wR0n3cm03S4FL1', // '93nF793e3PWaW4UW301r1c82y78e2c2U',
+  apiKey: '99fo93qB1U2RkI6B77TR9u7gKI697QZS', // '93nF793e3PWaW4UW301r1c82y78e2c2U',
   // API URL
-  baseUrl: 'http://82.202.226.53/api/',
+  baseUrl: 'http://82.202.226.53/api/4.0/',
   // SITE URL
   siteUrl: 'http://82.202.226.53/',
   // SITE LOGO
