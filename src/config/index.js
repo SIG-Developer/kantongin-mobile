f41d04d0baf8273e9810124dd634ca882415fceb
Melekesov Aleksey <i198622@gmail.com>
2017-10-02 22:07:58 +0400

    Refactor app config.

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,13 @@
-let config = require('./production');
-if (__DEV__) {
-  config = require('./development');
-}
-export default config.default;
+export default {
+  username: 'admin@example.com', // admin@example.com
+  // API KEY
+  apiKey: '9233nq042s03RvSg7928C511wPy2s971', // '93nF793e3PWaW4UW301r1c82y78e2c2U',
+  // API URL
+  baseUrl: 'http://82.202.226.53/api/4.0/',
+  // SITE URL
+  siteUrl: 'http://82.202.226.53/',
+  // SITE LOGO
+  logoUrl: 'http://82.202.226.53/images/logos/1/cart.png',
+  // SHOP NAME
+  shopName: 'CS-Cart',
+};
