6f13e2acd3f1896e54fbfc3bb994a5018ab6ac66
Melekesov Aleksey <i198622@gmail.com>
2017-10-16 21:07:25 +0400

    Refactor locales.

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,16 +1,17 @@
 import DeviceInfo from 'react-native-device-info';
 import gettext from 'gettext.js';
 
-const langs = ['ru'];
+const langs = ['ru', 'en'];
 let jsonData;
 const locale = DeviceInfo.getDeviceLocale().split('-')[0];
 
 if (langs.includes(locale)) {
   switch (locale) {
     case 'ru':
-      jsonData = require('../locales/ru');
+      jsonData = require('../config/locales/ru.json');
       break;
     default:
+      jsonData = require('../config/locales/en.json');
   }
 
   gettext.setLocale(locale);
