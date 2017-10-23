b1a20469ca48f8790bbc0ea6bc1bb6aeb4bd8bfb
Melekesov Aleksey <i198622@gmail.com>
2017-10-23 22:21:02 +0400

    Fix config.

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   username: 'admin@example.com', // admin@example.com
   // API KEY
-  apiKey: 'i1K6SK48sh2Q4Mf15SGW992WK4N2xn7v', // '93nF793e3PWaW4UW301r1c82y78e2c2U',
+  apiKey: '95380b2e1X91hn7n969U93gU9701F1f3', // '93nF793e3PWaW4UW301r1c82y78e2c2U',
   // API URL
   baseUrl: 'http://82.202.226.53/api/4.0/',
   // SITE URL
