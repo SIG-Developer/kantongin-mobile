8482d35673dfba588833bac867b0d01bfe9056d6
Melekesov Aleksey <i198622@gmail.com>
2017-12-07 21:48:42 +0400

    Added cart tests.

--- __tests__/reducers/cart.spec.js
+++ __tests__/reducers/cart.spec.js
@@ -6,16 +6,9 @@ import {
   CART_CONTENT_SUCCESS,
   CART_CONTENT_SAVE,
 
-  ADD_TO_CART_REQUEST,
-  ADD_TO_CART_SUCCESS,
-  ADD_TO_CART_FAIL,
-
-  CART_CLEAR_REQUEST,
   CART_CLEAR_SUCCESS,
-  CART_CLEAR_FAIL,
 
   CHANGE_AMOUNT,
-  AUTH_LOGOUT,
 } from '../../src/constants';
 
 
@@ -44,14 +37,85 @@ describe('Cart reducer', () => {
     });
   });
 
+
   it('should handle CART_SUCCESS', () => {
     expect(reducer(undefined, {
       type: CART_SUCCESS,
       payload: {
         products: [{ product_id: 1, }],
+        payments: {},
       }
     })).toMatchObject({
       products: [{ product_id: 1, }],
+    });
+  });
+
+
+  it('should handle CART_CLEAR_SUCCESS', () => {
+    expect(reducer(undefined, {
+      type: CART_CLEAR_SUCCESS,
+      payload: {}
+    })).toMatchObject({
+      amount: 0,
+      products: {},
+      fetching: false,
+    });
+  });
+
+
+  it('should handle CART_CONTENT_SUCCESS', () => {
+    expect(reducer(undefined, {
+      type: CART_CONTENT_SUCCESS,
+      payload: {
+        user_data: {
+          username: 'name',
+        },
+      },
+    })).toMatchObject({
+      user_data: {
+        username: 'name'
+      }
+    });
+  });
+
+
+  it('should handle CART_CONTENT_SAVE', () => {
+    expect(reducer({
+      user_data: {
+        username: 'name',
+      }
+    }, {
+      type: CART_CONTENT_SAVE,
+      payload: {
+        username: 'new_name',
+      },
+    })).toMatchObject({
+      user_data: {
+        username: 'new_name'
+      }
+    });
+  });
+
+
+  it('should handle CHANGE_AMOUNT', () => {
+    expect(reducer({
+      products: {
+        1: {
+          amount: 1,
+        }
+      },
+    }, {
+      type: CHANGE_AMOUNT,
+      payload: {
+        cid: 1,
+        amount: 3,
+      },
+    })).toMatchObject({
+      products: {
+        1: {
+          amount: 3,
+        }
+      },
     });
   });
 });
