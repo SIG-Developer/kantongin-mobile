e27856f2db94287f7b7e045261c2dfe6af6a7d95
Melekesov Aleksey <i198622@gmail.com>
2018-02-27 21:59:12 +0400

    Added search tests.

---
+++ __tests__/reducers/search.spec.js
@@ -0,0 +1,66 @@
+import reducer from '../../src/reducers/search';
+import {
+  SEARCH_PRODUCTS_REQUEST,
+  SEARCH_PRODUCTS_FAIL,
+  SEARCH_PRODUCTS_SUCCESS,
+} from '../../src/constants';
+
+
+describe('Search reducer', () => {
+  it('should return the initial state', () => {
+    const initialState = {
+      params: {
+        page: 1,
+      },
+      items: [],
+      fetching: true,
+    };
+
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+
+  it('should handle SEARCH_PRODUCTS_REQUEST', () => {
+    expect(reducer(undefined, {
+      type: SEARCH_PRODUCTS_REQUEST,
+    })).toMatchObject({
+      fetching: true,
+    });
+  });
+
+
+  it('should handle SEARCH_PRODUCTS_SUCCESS', () => {
+    expect(reducer(undefined, {
+      type: SEARCH_PRODUCTS_SUCCESS,
+      payload: {
+        products: [
+          {
+            id: 1,
+          }
+        ],
+        params: {
+          page: 1,
+        },
+      }
+    })).toMatchObject({
+      fetching: false,
+      items: [
+        {
+          id: 1,
+        },
+      ],
+      params: {
+        page: 1,
+      }
+    });
+  });
+
+
+  it('should handle SEARCH_PRODUCTS_FAILS', () => {
+    expect(reducer(undefined, {
+      type: SEARCH_PRODUCTS_FAIL,
+    })).toMatchObject({
+      fetching: false,
+    });
+  });
+});
