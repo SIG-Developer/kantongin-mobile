9366d7ac94390fcf60fff46fbeba2f4ce21b801f
Алексей Мелекесов <i198622@gmail.com>
2018-07-30 22:14:28 +0400

    Remove debug code.

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -25,12 +25,8 @@ import * as wishListActions from './wishListActions';
 export function deviceInfo(data) {
   return (dispatch) => {
     dispatch({ type: REGISTER_DEVICE_REQUEST });
-    console.log(data, 'token send');
-    alert('token send ' + data.token);
     return Api.post('/sra_notifications', data)
       .then((response) => {
-        alert('token send ' + data.token);
-        console.log(response, data);
         dispatch({
           type: REGISTER_DEVICE_SUCCESS,
           payload: {
