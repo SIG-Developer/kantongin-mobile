f79aab56d841e2029d420e3be3f093152d1ff2c8
Алексей Мелекесов <i198622@gmail.com>
2018-08-29 22:00:27 +0400

    Added cc_outside.tpl payment template.

--- src/screens/CheckoutPayment.js
+++ src/screens/CheckoutPayment.js
@@ -22,6 +22,7 @@ import CartFooter from '../components/CartFooter';
 import FormBlock from '../components/FormBlock';
 import PaymentPhoneForm from '../components/PaymentPhoneForm';
 import PaymentCreditCardForm from '../components/PaymentCreditCardForm';
+import PaymentCreditCardOutside from '../components/PaymentCreditCardOutside';
 import PaymentCheckForm from '../components/PaymentCheckForm';
 import PaymentPaypalForm from '../components/PaymentPaypalForm';
 import Spinner from '../components/Spinner';
@@ -76,6 +77,7 @@ const styles = EStyleSheet.create({
 });
 
 const PAYMENT_CREDIT_CARD = 'views/orders/components/payments/cc.tpl';
+const PAYMENT_CREDIT_CARD_OUTSIDE = 'views/orders/components/payments/cc_outside.tpl';
 const PAYMENT_CHECK = 'views/orders/components/payments/check.tpl';
 const PAYMENT_PAYPAL_EXPRESS = 'addons/paypal/views/orders/components/payments/paypal_express.tpl';
 const PAYMENT_PHONE = 'views/orders/components/payments/phone.tpl';
@@ -276,6 +278,15 @@ class CheckoutStepThree extends Component {
     let form = null;
     // FIXME: HARDCODE
     switch (selectedItem.template) {
+      case PAYMENT_CREDIT_CARD_OUTSIDE:
+        form = (
+          <PaymentCreditCardOutside
+            onInit={(ref) => {
+              this.paymentFormRef = ref;
+            }}
+          />
+        );
+        break;
       case PAYMENT_CREDIT_CARD:
         form = (
           <PaymentCreditCardForm
