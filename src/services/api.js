7b00315165dcc459845798ba832c6bba53959b91
Алексей Мелекесов <i198622@gmail.com>
2018-10-10 20:18:29 +0400

    Fix api response timeout.

--- src/services/api.js
+++ src/services/api.js
@@ -11,7 +11,7 @@ const sl = DeviceInfo.getDeviceLocale().split('-')[0];
 // Config axios defaults.
 const AxiosInstance = axios.create({
   baseURL: config.baseUrl,
-  timeout: 15000,
+  timeout: 100000,
   params: {
     sl,
     items_per_page: 20,
